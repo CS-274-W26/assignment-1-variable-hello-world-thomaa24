3c6aac4743651259894eacd818a50d4af48d5b7f
Anna Thomas <thomaa24@oregonstate.edu>
2026-01-15 16:38:01 -0800

    completed code

--- prog1.c
+++ prog1.c
@@ -1,11 +1,24 @@
 #include <stdio.h> // Necessary for printf()
+#include <stdlib.h>
+#include <time.h> // for generating pseudorandom numbers
 
-// TODO Other includes
 
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
+	srand(time(NULL)); // seed random number generator
+
+	// generate random number between 0 and 10 (inclusive)
+	int myrand = rand() % 11;
 	
-	// TODO Complete the program
+	// based on generated number print a statement
+	if (myrand < 5) {
+		printf("Eat more beef, kick less cats \n");
+	} else if (myrand < 10) {
+		printf("FRODO LIVES \n");
+	} else {
+		printf("Larn is the best roguelike \n");
+	}
+
+	// print the random number
+	printf("The random number was: %d\n", myrand);
 }
